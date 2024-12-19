8fb098dde6b9279621a26ffc1944a84e595bdaf9
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-12-19 23:18:39 +0100

    Minor edit

--- nodeanalizerandfixer.sh
+++ nodeanalizerandfixer.sh
@@ -201,7 +201,8 @@ if [[ -n $FLUXOS_VERSION ]]; then
 
   if [[ -f /var/log/sas-error.log ]]; then
     error_last_line=$(sudo cat /var/log/sas-error.log | tail -n1)
-    echo -e "${X_MARK} ${CYAN}Flux System Attestation Service (SAS) /var/log/sas-error.log detected!${NC}"
+    echo -e "${WORNING} ${CYAN}Flux System Attestation Service (SAS) error detected!${NC}"
+    echo -e "${PIN} ${CYAN}Log file: /var/log/sas-error.log${NC}"
     echo -e "${PIN} ${CYAN}Last error line: $error_last_line${NC}"
     echo -e ""
     exit
